cc2681c4ab7395877ad3eb2fd9b5d6fdd63de44c
罗正龙 <luozhenglong@luozhenglongdeiMac.local>
2016-06-12 10:22:09 +0800

    refine drawable view position

--- app/src/main/java/com/example/dell/myapplication/CustomDrawableView.java
+++ app/src/main/java/com/example/dell/myapplication/CustomDrawableView.java
@@ -2,9 +2,14 @@ package com.example.dell.myapplication;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by zhenglong on 6/11/16.
@@ -15,14 +20,19 @@ public class CustomDrawableView extends View {
     public CustomDrawableView(Context context) {
         super(context);
 
-        int x = 0;
-        int y = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point size = new Point();
+        windowManager.getDefaultDisplay().getSize(size);
         int width = 300;
         int height = 50;
+        int x = (size.x - width) / 2;
+        int y = (size.y - height) / 2;
 
         _drawable = new ShapeDrawable(new OvalShape());
         _drawable.getPaint().setColor(0xff74AC23);
         _drawable.setBounds(x, y, x + width, y + height);
+
+//        this.setBackgroundColor(Color.BLACK);
     }
 
     @Override
